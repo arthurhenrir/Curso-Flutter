f3e2a6f6a188a3a462dd8046da89e0aba0dcf039
Arthur Henrique <87656157+black258br@users.noreply.github.com>
2022-09-28 13:24:17 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget
-{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    Text texto = Text('HMMMM');
+    TextField campo = TextField();
+
     return MaterialApp(
       title: 'My App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('teste'),
-        ),
-        body: Center(
-          child: Text('ola'),
-        )
-      ),
+          appBar: AppBar(
+            title: Text('teste'),
+            centerTitle: true,
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(12),
+            child: Column(
+              children: <Widget>[
+                texto,
+                campo,
+                Text('Teste 2', style: TextStyle(color: Colors.amber)),
+                Row(
+                  children: <Widget>[
+                    Text('01'),
+                    Text('02'),
+                  ],
+                ),
+                OutlinedButton(
+                  onPressed: null,
+                  child: Text('clique-me'),
+                )
+              ],
+            ),
+          )),
     );
   }
-  
 }
