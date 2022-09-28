fdf7c5d9a7119a613e77ca54f9b3acc529c6af21
Arthur Henrique <87656157+black258br@users.noreply.github.com>
2022-09-28 14:00:59 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,51 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget
-{
+class App extends StatefulWidget {
+  _App createState() => _App();
+}
+
+class _App extends State<App> {
+  int totalClicks = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
+      title: 'Meu app',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('teste'),
-        ),
-        body: Center(
-          child: Text('ola'),
-        )
-      ),
+          appBar: AppBar(
+            title: Text('Meu aplicativo'),
+            centerTitle: true,
+            backgroundColor: Colors.blue,
+          ),
+          body: Center(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Total de cliques: $totalClicks",
+                style: TextStyle(color: Colors.amberAccent),
+              ),
+              ElevatedButton(
+                  onPressed: _add,
+                  child: Text('Clicar', style: TextStyle(color: Colors.black)),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.amberAccent,
+                  )),
+            ],
+          )),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _add,
+            child: Icon(Icons.add),
+            backgroundColor: Colors.amberAccent,
+          )),
     );
   }
-  
+
+  void _add() {
+    setState(() {
+      totalClicks++;
+    });
+  }
 }
